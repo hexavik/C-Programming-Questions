3aec07cc6c5c423eddca4589abc9cac5ef169317
hexavik <vikrant_ap@hotmail.com>
2025-01-19 16:42:47 +0530

    Add `Multiple Duplicates Identifier` Solution

---
+++ Arrays/Multiple_Duplicates_Identifier/multiple_duplicates_identifier.c
@@ -0,0 +1,74 @@
+/**
+ * @file multiple_duplicates_identifier.c
+ *
+ * @author Vikrant A. P. (vikrant_ap@hotmail.com)
+ *
+ * @date 2025-01-19
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // Input array
+    int arr[] = {4, 3, 2, 7, 8, 2, 3, 1};
+
+    // Calculate the size of the array
+    int arr_len = sizeof(arr) / sizeof(arr[0]);
+
+    if (!arr_len) {
+        printf("Input array is invalid (size zero)\n");
+        exit(1);
+    }
+
+    // Find the maximum element in the array
+    int max_element = arr[0];
+    for (int i = 1; i < arr_len; ++i) {
+        if (arr[i] > max_element) {
+            max_element = arr[i];
+        }
+    }
+
+    // Create a hash table dynamically sized
+    int * duplicate_elements = (int * )calloc(max_element + 1, sizeof(arr[0]));
+    if (duplicate_elements == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+
+    // Output array to store duplicates
+    int * duplicates = (int * )malloc(arr_len * sizeof(arr[0]));
+    if (!duplicates) {
+        printf("Memory allocation failed\n");
+        free(duplicate_elements);
+        duplicate_elements = NULL;
+        exit(1);
+    }
+
+    // Index of the output array
+    int duplicate_arr_index = 0;
+
+    for (int i = 0; i < arr_len; ++i) {
+        duplicate_elements[arr[i]]++;
+
+        // If the occurrence of the element is twice thenstore the 
+        // element in the output array. Do not repeat the duplicated
+        // element.
+        if (duplicate_elements[arr[i]] == 2) {
+            duplicates[duplicate_arr_index] = arr[i];
+            duplicate_arr_index++;
+        }
+    }
+
+    // Display the output array
+    printf("result: [");
+    for (int i = 0; i < duplicate_arr_index; ++i) {
+        printf("%d, ", duplicates[i]);
+    }
+    printf("]\n");
+
+    return 0;
+}
---
+++ Arrays/Multiple_Duplicates_Identifier/multiple_duplicates_identifier.md
@@ -0,0 +1,36 @@
+# Mutliple Duplicates Identifier
+
+**Problem Statement:**
+
+Given an integer array, design a program to identify all the numbers that appear more than once in the array.
+
+**Requirements:**
+
+Write a function or program that takes an integer array as input and returns a list of numbers that have duplicates in the array.
+
+**Input:**
+
+A single integer array `arr` of size `n`, where `n≥1`.
+
+```bash
+arr = [4, 3, 2, 7, 8, 2, 3, 1]
+```
+
+**Output:**
+
+An array or list of integers representing the numbers that appear more than once in the input array.
+
+```bash
+result = [2, 3]
+```
+
+**Constraints:**
+
+- The input array may contain both positive and negative integers.
+- The output list should only include each duplicate number once, regardless of how many times it appears.
+- If no duplicates are found, the output should be an empty list.
+
+**Notes:**
+
+- Aim for an efficient solution in terms of time and space complexity. If extra space is allowed, consider using a hash table or dictionary.
+- For an **in-place** solution, consider using the input array itself for marking visited elements.
